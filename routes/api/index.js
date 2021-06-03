697d076587f1787dafb3f8d80ba49952e15558d9
D.L. White - Vunte Ron <dlwhite112@gmail.com>
2021-06-03 11:00:32 -0500

    add homeRoutes

---
+++ routes/api/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const workoutRoutes = require('./workoutRoutes');
+
+router.use('/', workoutRoutes);
+
+module.exports = router;
---
+++ routes/homeRoutes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const path = require("path");
+
+router.get("/", async (req, res) => {
+  try {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  } catch (err) {
+    res.status(400);
+  }
+});
+router.get("/exercise", async (req, res) => {
+  try {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  } catch (err) {
+    res.status(400);
+  }
+});
+router.get("/status", async (req, res) => {
+  try {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  } catch (err) {
+    res.status(400);
+  }
+});
+module.exports = router
